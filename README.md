4d59d4ed18b53a66488d3989dbdab5d3c0ddf341
Muhammad Tarique <muhammadtarique332@gmail.com>
2026-01-06 10:27:53 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CodeAlpha_Cgpa_Calculator
+
---
+++ cgpa_calculator.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    // ===== Step 1: Declare Variables =====
+    int totalCourses;
+
+    string courseName[20];
+    string grade[20];
+    double credit[20];
+
+    double totalPoints = 0;
+    double totalCredits = 0;
+
+    cout << "====== CGPA CALCULATOR ======" << endl;
+    cout << "Enter total number of courses: ";
+    cin >> totalCourses;
+
+     // ===== Step 2: Take Input for Each Course =====
+    for (int i = 0; i < totalCourses; i++) {
+
+        cout << "\nCourse " << i + 1 << endl;
+
+        cout << "Enter course name: ";
+        cin >> courseName[i];
+
+        cout << "Enter grade (A, A-, B+, B, C+, C, D, F): ";
+        cin >> grade[i];
+
+        cout << "Enter credit hours: ";
+        cin >> credit[i];
+
+        // ===== Step 3: Convert Grade to Points =====
+        double gradePoint = 0;
+
+        if (grade[i] == "A")
+            gradePoint = 4.0;
+        else if (grade[i] == "A-")
+            gradePoint = 3.7;
+        else if (grade[i] == "B+")
+            gradePoint = 3.3;
+        else if (grade[i] == "B")
+            gradePoint = 3.0;
+        else if (grade[i] == "C+")
+            gradePoint = 2.3;
+        else if (grade[i] == "C")
+            gradePoint = 2.0;
+        else if (grade[i] == "D")
+            gradePoint = 1.0;
+        else if (grade[i] == "F")
+            gradePoint = 0.0;
+        else
+            cout << "Invalid grade! 0 points counted.\n";
+
+        // ===== Step 4: Calculate Points =====
+        totalPoints += gradePoint * credit[i];
+        totalCredits += credit[i];
+    }
+
+    // ===== Step 5: Calculate Semester GPA =====
+    double semesterGPA = totalPoints / totalCredits;
+
+    // ===== Step 6: Previous CGPA (Optional) =====
+    char choice;
+    double finalCGPA = semesterGPA;
+
+    cout << "\nDo you have previous CGPA? (y/n): ";
+    cin >> choice;
+
+    if (choice == 'y' || choice == 'Y') {
+        double oldCGPA, oldCredits;
+
+        cout << "Enter previous CGPA: ";
+        cin >> oldCGPA;
+
+        cout << "Enter previous total credits: ";
+        cin >> oldCredits;
+
+        double oldPoints = oldCGPA * oldCredits;
+        finalCGPA = (oldPoints + totalPoints) / (oldCredits + totalCredits);
+    }
+
+    // ===== Step 7: Display Result =====
+    cout << "\n====== RESULT ======" << endl;
+
+    for (int i = 0; i < totalCourses; i++) {
+        cout << courseName[i]
+             << " | Grade: " << grade[i]
+             << " | Credits: " << credit[i] << endl;
+    }
+
+    cout << "\nSemester GPA: " << semesterGPA << endl;
+    cout << "Overall CGPA: " << finalCGPA << endl;
+    cout << "====================" << endl;
+
+    return 0;
+}
